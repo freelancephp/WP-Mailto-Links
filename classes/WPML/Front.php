fa0ecd09759666fcf879ffbedc6d94c34b6fe7f0
Victor Villaverde Laan <info@freelancephp.net>
2015-06-22 01:54:52 +0200

    Option for strong input field protection (javascript or antispambot)

--- classes/WPML/Front.php
+++ classes/WPML/Front.php
@@ -262,14 +262,18 @@ class WPML_Front
 
         $encodedEmail = $this->getEncEmail($email);
 
-        // add data-enc-email after "<input"
-        $encodedInput = '';
-        $encodedInput .= substr($input, 0, 6);
-        $encodedInput .= ' data-enc-email="' . $encodedEmail . '"';
-        $encodedInput .= substr($input, 6);
+        if ($this->optionValues['input_strong_protection'] == 1) {
+            // add data-enc-email after "<input"
+            $encodedInput .= substr($input, 0, 6);
+            $encodedInput .= ' data-enc-email="' . $encodedEmail . '"';
+            $encodedInput .= substr($input, 6);
 
-        // remove email from value attribute
-        $encodedInput = str_replace($email, '', $encodedInput);
+            // remove email from value attribute
+            $encodedInput = str_replace($email, '', $encodedInput);
+        } else {
+            // replace email in value attribute
+            $encodedInput = str_replace($email, antispambot($email), $input);
+        }
 
         return $encodedInput;
     }
