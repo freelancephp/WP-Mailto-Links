7fb3b1c5ac5a27fc0dd21b992f9539ff08856e90
Victor Villaverde Laan <info@freelancephp.net>
2015-12-11 21:18:25 +0100

    make static for activation hook

--- src/classes/WPML.php
+++ src/classes/WPML.php
@@ -62,9 +62,29 @@ final class WPML extends WPDev_Plugin
      * Upgrade procedure
      * Convert old to new option values
      */
-    public function upgrade()
+    public static function upgrade()
     {
-        $option = $this->get('option');
+        $option = self::glob('option');
+
+        $defaultValues = array(
+            'version' => null,
+            'convert_emails' => 1,
+            'protect' => 1,
+            'filter_body' => 1,
+            'filter_posts' => 1,
+            'filter_comments' => 1,
+            'filter_widgets' => 1,
+            'filter_rss' => 1,
+            'filter_head' => 1,
+            'input_strong_protection' => 0,
+            'protection_text' => '*protected email*',
+            'icon' => 0,
+            'image_no_icon' => 0,
+            'no_icon_class' => 'no-mail-icon',
+            'class_name' => 'mail-link',
+            'widget_logic_filter' => 0,
+            'own_admin_menu' => 0,
+        );
 
         // get old option name "WP_Mailto_Links_options"
         $oldOption = new WPDev_Option('WP_Mailto_Links_options', $defaultValues);
@@ -91,7 +111,6 @@ final class WPML extends WPDev_Plugin
             $option->update();
             $oldOption->delete();
         }
-
     }
 
     /**
