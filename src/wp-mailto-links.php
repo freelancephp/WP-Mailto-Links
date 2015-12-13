979ad4ad8d1eca5bcf15368d7288ea948a5ff810
Victor Villaverde Laan <info@freelancephp.net>
2015-12-13 14:01:49 +0100

    test constant

--- src/wp-mailto-links.php
+++ src/wp-mailto-links.php
@@ -4,27 +4,29 @@ Plugin Name:    WP Mailto Links - Manage Email Links
 Plugin URI:     http://www.freelancephp.net/wp-mailto-links-plugin
 Description:    Manage mailto links on your site and protect email addresses from spambots, set mail icon and more.
 Author:         Victor Villaverde Laan
-Version:        1.6.0
+Version:        2.0.0
 Author URI:     http://www.freelancephp.net
 License:        Dual licensed under the MIT and GPL licenses
 Text Domain:    wp-mailto-links
 Domain Path:    /languages
 */
+if (!defined('WP_MAILTO_LINKS_FILE')) {
+    define('WP_MAILTO_LINKS_FILE', __FILE__);
+}
 
 // autoloader
 if (!class_exists('WPDev_Loader')) {
     require_once realpath(__DIR__ . '/classes/WPDev/Loader.php');
 }
-
 WPDev_Loader::register();
 WPDev_Loader::addPath(__DIR__ . '/classes');
 
 // start plugin
 WPML::create(array(
     'key'   => 'wp-mailto-links',
-    'FILE'  => defined('WPML_DEV_FILE') ? constant('WPML_DEV_FILE') : __FILE__,
+    'FILE'  => WP_MAILTO_LINKS_FILE,
     'DIR'   => __DIR__,
-    'URL'   => defined('WPML_DEV_FILE') ? plugins_url('', constant('WPML_DEV_FILE')) : plugins_url('', __FILE__),
+    'URL'   => plugins_url('', WP_MAILTO_LINKS_FILE),
 ));
 
 /*?>*/
