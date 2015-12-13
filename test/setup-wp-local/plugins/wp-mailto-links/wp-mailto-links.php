a9ceb3cd13f905538d559b673824e2fb3406ae54
Victor Villaverde Laan <info@freelancephp.net>
2015-12-13 14:01:34 +0100

    add plugin setup test environment

---
+++ test/setup-wp-local/plugins/wp-mailto-links/wp-mailto-links.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: WP Mailto Links
+Plugin URI: http://www.freelancephp.net/wp-mailto-links-plugin
+Description: DEV version
+Author: Victor Villaverde Laan
+Version: 1000
+Author URI: http://www.freelancephp.net
+License: Dual licensed under the MIT and GPL licenses
+*/
+define('WP_MAILTO_LINKS_FILE', __FILE__);
+
+// include plugin
+require_once($_SERVER['DOCUMENT_ROOT'] . 'git/WP-Mailto-Links/src/wp-mailto-links.php');
