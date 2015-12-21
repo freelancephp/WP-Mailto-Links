e89a0f08bd3f1f9db4eff24aadef26ba9ea99208
Victor Villaverde Laan <info@freelancephp.net>
2015-12-21 23:49:57 +0100

    remove old code

--- src/classes/WPML/Admin.php
+++ src/classes/WPML/Admin.php
@@ -21,7 +21,8 @@ final class WPML_Admin extends WPDev_Admin_Page_MetaBox_04
      */
     public function __construct()
     {
-        add_action('init', array($this, 'createAdminPage'));
+        $this->createAdminPage();
+
         add_action('admin_enqueue_scripts', array($this, 'enqueueScripts'));
     }
 
@@ -118,77 +119,6 @@ final class WPML_Admin extends WPDev_Admin_Page_MetaBox_04
         );
 
         $this->adminPage = $adminPage;
-
-
-//        $templatesBasePath = WPML::glob('DIR') . '/templates/admin/page';
-//        $globals = WPML::plugin()->getAllGlobals();
-//        $mainMenu = (bool) WPML::glob('option')->getValue('own_admin_menu');
-//
-//        // create page
-//        $this->adminPage = new WPDev_Admin_Page_MetaBox(array(
-//            'id'              => $globals['key'] . '-option-page',
-//            'title'           => __('WP Mailto Links', 'wp-mailto-links'),
-//            'menuTitle'       => __('Mailto Links', 'wp-mailto-links'),
-//            'parentSlug'      => $mainMenu ? null : 'options-general.php',
-//            'iconUrl'         => 'dashicons-email',
-//            'defaultColumns'  => 2,
-//            'maxColumns'      => 2,
-//            'pageTemplate'    => $templatesBasePath . '/page.php',
-//            'templateVars'    => $globals,
-//        ));
-//
-//        // create meta boxes
-//        new WPDev_Admin_MetaBoxes(
-//            array(
-//                'mail-icon' => array(
-//                    'title' => __('Mail Icon', 'wp-mailto-links'),
-//                    'context' => 'normal',
-//                 ),
-//                'additional-classes' => array(
-//                    'title' => __('Additional Classes', 'wp-mailto-links'),
-//                    'context' => 'side',
-//                 ),
-//                'admin' => array(
-//                    'title' => __('Admin', 'wp-mailto-links'),
-//                    'context' => 'normal',
-//                 ),
-//                'support' => array(
-//                    'title' => __('Support', 'wp-mailto-links'),
-//                    'context' => 'side',
-//                 ),
-//            )
-//            , array(
-//                'adminPage'     => $this->adminPage,
-//                'templatesPath' => $templatesBasePath . '/meta-boxes',
-//                'templateVars'  => $globals,
-//            )
-//        );
-//
-//        // create help tabs
-//        new WPDev_Admin_HelpTabs(
-//            array(
-//                'general' => array(
-//                    'title' => __('General', 'wp-mailto-links'),
-//                 ),
-//                'shortcodes' => array(
-//                    'title' => __('Shortcode', 'wp-mailto-links'),
-//                 ),
-//                'template-tags' => array(
-//                    'title' => __('Template Tags', 'wp-mailto-links'),
-//                 ),
-//                'filter-hook' => array(
-//                    'title' => __('Filter Hook', 'wp-mailto-links'),
-//                 ),
-//                'action-hook' => array(
-//                    'title' => __('Action Hook', 'wp-mailto-links'),
-//                 ),
-//            )
-//            , array(
-//                'adminPage'     => $this->adminPage,
-//                'templatesPath' => $templatesBasePath . '/help-tabs',
-//                'templateVars'  => $globals,
-//            )
-//        );
     }
 
 }
