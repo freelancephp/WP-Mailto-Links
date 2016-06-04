905953ebb30b890974d4ff12fbc1647fb15393b0
Victor Villaverde Laan <info@freelancephp.net>
2016-06-05 01:19:29 +0200

    update version

--- wp-mailto-links/classes/WPML/TextDomain.php
+++ wp-mailto-links/classes/WPML/TextDomain.php
@@ -4,7 +4,7 @@
  *
  * @package  WPML
  * @category WordPress Plugins
- * @version  2.1.1
+ * @version  2.1.3
  * @author   Victor Villaverde Laan
  * @link     http://www.freelancephp.net/
  * @link     https://github.com/freelancephp/WP-Mailto-Links
