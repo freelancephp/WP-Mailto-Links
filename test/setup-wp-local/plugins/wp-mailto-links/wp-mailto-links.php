6a950c37cea24a2c5faabac39d968fe6620705e9
Victor <info@finewebdev.com>
2017-02-26 15:12:23 +0100

    change local setup locations

--- test/setup-wp-local/plugins/wp-mailto-links/wp-mailto-links.php
+++ test/setup-wp-local/plugins/wp-mailto-links/wp-mailto-links.php
@@ -1,14 +1,15 @@
 <?php
 /*
-Plugin Name: WP Mailto Links
-Plugin URI: http://www.freelancephp.net/wp-mailto-links-plugin
-Description: DEV version
-Author: Victor Villaverde Laan
-Version: 1000
-Author URI: http://www.freelancephp.net
-License: Dual licensed under the MIT and GPL licenses
+Plugin Name:    WP Mailto Links
+Plugin URI:     http://www.freelancephp.net/wp-mailto-links-plugin
+Description:    DEV version
+Author:         Victor Villaverde Laan
+Version:        1000
+Author URI:     http://www.freelancephp.net
+License:        Dual licensed under the MIT and GPL licenses
 */
 define('WP_MAILTO_LINKS_FILE', __FILE__);
 
-// include plugin
-require_once($_SERVER['DOCUMENT_ROOT'] . 'git/WP-Mailto-Links/wp-mailto-links/wp-mailto-links.php');
+
+// include plugin file (DEV version)
+require_once(dirname( __FILE__ ) . '/../../../../../wp-plugins/wpml/WP-Mailto-Links/wp-mailto-links/wp-mailto-links.php');
